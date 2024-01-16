3cea8bf66669cd968b8c64298e94ef8e2ff3b4f5
HarshVardhanK35 <kasa.saiharshavardhan@gmail.com>
2024-01-16 20:02:58 +0530

    Created App.JS

--- js/app.js
+++ js/app.js
@@ -1,20 +1,39 @@
+// ---------------------- Part - 01 ----------------------
 class CalorieTracker {
   constructor(){
     this._calorieLimit = 2000;
     this._totalCalories = 0;
     this._meals = [];
     this._workouts = [];
+
+    this._displayCaloriesTotal()
   }
 
   // Public Methods...
   addMeal(meal){
     this._meals.push(meal);
     this._totalCalories += meal.calories;
+
+    this._render()
   }
   addWorkout(workout){
     this._Workouts.push(workout);
     this._totalCalories -= workout.calories;
+
+    this._render()
   }
+
+  // ---------------------- Part - 02 ----------------------
+  // Private Methods
+  _displayCaloriesTotal(){
+    const totalCaloriesEl = document.getElementById('calories-total');
+    totalCaloriesEl.innerHTML = this._totalCalories;
+  }
+
+  _render(){
+    this._displayCaloriesTotal()
+  }
+
 }
 
 class Meal {
@@ -35,9 +54,13 @@ class Workout {
   }
 }
 
+// Created a new instance of CalorieTracker() object
 const tracker = new CalorieTracker();
+
+// Created a new Instance of Meal()
 const breakfast = new Meal('Breakfast', 300);
 tracker.addMeal(breakfast);
+
+// Created a new Instance of Workout()
 const pushups = new Workout('pushups', 200);
 tracker.addWorkout = pushups;
-
